85d49dde8401c8f8b8ff97e1a8c2696664fd760a
fernando <fernando.montero.perez@gmail.com>
2020-02-12 17:22:28 +0000

    Initial dump. Kinda like a commit, but the code is kinda useless thus categorized a dump

---
+++ src/js/script.js
@@ -0,0 +1,9 @@
+console.log("hello");
+    myFn = () => {
+        document.getElementById('portada').classList.toggle('disco-fuera');
+    }
+
+    playMusic = () => {
+        document.body.classList.toggle('crowd');
+
+    }
